96a11a82de27993d1ce81cd608a9704829e90c41
Br1nks <Br1nks@users.noreply.github.com>
2019-01-28 17:30:09 +0100

    iuhgib

--- Code/main.py
+++ Code/main.py
@@ -1,19 +1,24 @@
 import numpy as np
-# import SciPy as sp
+# import scipy as sp
 # import matplotlib as mpl
 
 germanCities = "GermanyCities.txt"
 hungaryCities = "HungaryCities.txt"
 sampleCities = 'SampleCoordinates.txt'
-mode1 = 'r'
 
 
 def read_coordinate_file(filename):
-    citiesCoordinates = open(filename, 'r')
-
+    mode1 = 'r'
+    citiesCoordinates = open(filename, mode1)
+    compList = np.array([])
+    print(np.shape(compList))
     for coord in citiesCoordinates:
-        a = np.array(coord)
-        print(a)
+        a = coord[1:-2:]
+        a = a.split(',')
+        a = np.array([float(a[0]), float(a[1])])
+        #compList = np.append(compList, a, axis=1)
+        compList = np.concatenate((compList, a), axis=1)
+    print(compList)
 
     citiesCoordinates.close
 
