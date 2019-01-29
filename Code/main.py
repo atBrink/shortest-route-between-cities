bec06ccdafa7aded8ab196f7fe2c544bb0540c07
Br1nks <Br1nks@users.noreply.github.com>
2019-01-29 11:21:15 +0100

    Finished part 1

--- Code/main.py
+++ Code/main.py
@@ -6,21 +6,35 @@ germanCities = "GermanyCities.txt"
 hungaryCities = "HungaryCities.txt"
 sampleCities = 'SampleCoordinates.txt'
 
+citiesRadius = 1
 
+
+# 1
 def read_coordinate_file(filename):
     mode1 = 'r'
     citiesCoordinates = open(filename, mode1)
-    compList = np.array([])
-    print(np.shape(compList))
+    coord_list = np.array([0, 0], ndmin=2)
+
     for coord in citiesCoordinates:
         a = coord[1:-2:]
         a = a.split(',')
-        a = np.array([float(a[0]), float(a[1])])
-        #compList = np.append(compList, a, axis=1)
-        compList = np.concatenate((compList, a), axis=1)
-    print(compList)
+        a = np.array([float(a[0]), float(a[1])], ndmin=2)
+        coord_list = np.concatenate([coord_list, a], axis=0)
+
+    # Delete the row that we initialized with.
+    coord_list = np.delete(coord_list, 0, axis=0)
 
     citiesCoordinates.close
+
+
+# 2
+def plot_points(coord_list):
+    print("hej")
+
+
+# 3
+def construct_graph_connections(coord_list, radius):
+    print("Hej")
 
 
 read_coordinate_file("SampleCoordinates.txt")
